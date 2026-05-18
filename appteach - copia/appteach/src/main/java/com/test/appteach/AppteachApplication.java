4d917c321d26725620d7ab74c73ffb2d8ee6b015
joserendonu <angelemilio4444@gmail.com>
2026-05-17 20:17:57 -0500

    primer commit o cambio inicial

---
+++ appteach - copia/appteach/src/main/java/com/test/appteach/AppteachApplication.java
@@ -0,0 +1,14 @@
+package com.test.appteach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppteachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppteachApplication.class, args);
+		System.out.println("ASDF");
+	}
+
+}
---
+++ appteach - copia/appteach/src/test/java/com/test/appteach/AppteachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.appteach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppteachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
